cb15fef34a96d2b31df3916c51b39633ecfeedf7
Isaac <isaac.faleiros.quevedo@gmail.com>
2026-03-10 12:32:28 -0300

    Exercícios php aula 1

---
+++ exercicios-php/cadastro.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nome = $_POST["nome"];
+    $password = $_POST["senha"];
+    $email = $_POST["email"];
+    $user = $_POST["user"];
+
+    echo "Sua conta foi criada $user"
+
+?>
---
+++ exercicios-php/ex5.php
@@ -0,0 +1,70 @@
+<!--
+Data: 10/03/2026
+Autor: Bruno L. de Lima; Isaac F. Quevedo
+Objetivo:
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoriais desses números.
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+</head>
+<body>
+    <form action="ex5.php" class="form" method="POST">
+        <label for="n1">Digite o 1º número: </label>
+        <input type="text" name="n1">
+        <br>
+        <label for="n2">Digite o 2º número: </label>
+        <input type="text" name="n2">
+        <br>
+        <label for="n3">Digite o 3º número: </label>
+        <input type="text" name="n3">
+        <br>
+        <label for="n4">Digite o 4º número: </label>
+        <input type="text" name="n4">
+        <br>
+        <label for="n5">Digite o 5º número: </label>
+        <input type="text" name="n5">
+        <br>
+        <button type="submit">Fazer soma dos fatoriais</button>
+    </form>
+
+<style>
+
+</style>
+</body>
+</html>
+
+<?php
+
+    $n1 = $_POST["n1"];
+    $n2 = $_POST["n2"];
+    $n3 = $_POST["n3"];
+    $n4 = $_POST["n4"];
+    $n5 = $_POST["n5"];
+    $lista_n = [$n1, $n2, $n3, $n4, $n5];
+
+    $fat = 0;
+    $soma = 0;
+
+    foreach($lista_n as $n){
+        for($i = 1; $i <= $n; $i++){
+            if($fat == 0 ){
+                $fat = 1;
+            }
+            else{
+                $fat *= $i;
+            }
+        }
+
+        $soma += $fat;
+        $fat = 0;
+    }
+
+    echo "A soma dos fatoriais de $n1, $n2, $n3, $n4, $n5 é igual a $soma";
+?>
